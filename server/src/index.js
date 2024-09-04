7017715b02c6089a5d2c5a3971a8265cf61db862
Sarthak_Kotia <sarthakkotia@gmail.com>
2024-09-04 23:23:35 +0530

    integrating mongodb uri to env and created seperate file for db connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,40 +1,48 @@
 // console.log("Hello, World!")
 // Packages
-require("dotenv").config()
-const express = require('express');
-const mongoose = require('mongoose');
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./env"
+})
+// import express from "express";
+// import mongoose from "mongoose";
 // from files
-const authRouter = require("./routes/auth.js");
-const adminRouter = require('./routes/admin.js');
-const productRouter = require('./routes/product.js');
-const userRouter = require('./routes/user.js');
+// const authRouter = require("./routes/auth.js");
+// const adminRouter = require('./routes/admin.js');
+// const productRouter = require('./routes/product.js');
+// const userRouter = require('./routes/user.js');
+import { connectDB } from "./db/db_connect.js";
+
+
+connectDB()
+const PORT = process.env.PORT || 3000;
+
+
 
 
 // Initalization
-const app = express();
-const PORT = process.env.PORT || 3000;
-const DB = "mongodb+srv://admin:admin@cluster0.vgqvht2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-// middleware
-app.use(express.json())
-app.use(authRouter);
-app.use(adminRouter);
-app.use(productRouter);
-app.use(userRouter)
+// const app = express();
+// // middleware
+// app.use(express.json())
+// app.use(authRouter);
+// app.use(adminRouter);
+// app.use(productRouter);
+// app.use(userRouter)
 
-//connections
-mongoose.connect(DB).then(() => {
-  console.log("Connection Successful");
-}).catch((e) => {
-  console.log(`Error Occured ${e}`)
-})
+// //connections
+// mongoose.connect(DB).then(() => {
+//   console.log("Connection Successful");
+// }).catch((e) => {
+//   console.log(`Error Occured ${e}`)
+// })
 
-app.listen(PORT, "0.0.0.0" ,() => {
-    console.log(`Connected at port : ${PORT}`);
-} )
+// app.listen(PORT, "0.0.0.0" ,() => {
+//     console.log(`Connected at port : ${PORT}`);
+// } )
 
-app.get('/', function (req, res) { 
-  res.json({"name": "sarthak"});
-})
+// app.get('/', function (req, res) { 
+//   res.json({"name": "sarthak"});
+// })
 
 // app.get("/hello-world", (_, res) => {
 //     res.send("Hello, World! 👋");
