f42cf4d53f05ce97a33b2c292ec3879e65c8b9d4
Sarthak_Kotia <sarthakkotia@gmail.com>
2024-09-15 22:54:52 +0530

    created register user endpoint need to test it

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,15 +1,36 @@
+import { User } from "../models/user.model.js";
+import { APIError } from "../utils/APIError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-
-// TODO: get name, email, password, address?, type
-// TODO: validate all these values
-// TODO: check if user already exists
-// TODO: create user
-// TODO: give response back
 
 const register_user = asyncHandler(
     async (req, res) => {
-        res.status(200).json({
-            message: "OK"
+        const {name, email, password, address} = req.body
+        if(!name || !email || !password){
+            return res.status(400),json({
+                message: "please enter valid arguments"
+            })
+        }
+        const existedUser = await User.find({email})
+        if(existedUser){
+            return res.status(400).json({
+                message: "email already exists"
+            })
+        }
+        const user = await User.create({
+            name,
+            email,
+            password,
+            type: "user",
+            address: address ? address: ""
+        })
+        const createdUser = await User.findById(user._id).select(
+            "-password"
+        )
+        if(!createdUser){
+            throw APIError("Server error to create user, please try again")
+        }
+        return res.status(201).json({
+            message: "User created successfully"
         })
     }
 )
